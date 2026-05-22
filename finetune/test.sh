9da065bcd39d5cb0f3a219d34d888ce83bbbab99
cxkxk <104539957+cxkxk@users.noreply.github.com>
2026-05-22 09:48:46 +0800

    Update test.sh

--- finetune/test.sh
+++ finetune/test.sh
@@ -1,8 +1,9 @@
 run_id=$1
+data_path=$2
 bsz=128
 
-CUDA_VISIBLE_DEVICES=0 torchrun --rdzv_backend=c10d --rdzv_endpoint=localhost:1750 --nnodes=1 --nproc_per_node=1 inter_res_bank_new.py \
-	--data-path /home/cxk/cxk/workplace/VisualSearch/visdial_1.0_train \
+CUDA_VISIBLE_DEVICES=0 torchrun --rdzv_backend=c10d --rdzv_endpoint=localhost:1750 --nnodes=1 --nproc_per_node=1 train.py \
+	--data-path $data_path \
 	--amp --run_id $run_id --batch-size $bsz --model_name $model_name --backbone_ft --test_model 
 
 	
